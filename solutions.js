b0b599ec3b1d0f8737aa313a72da4428a0c728d5
Johnny Delgado <johnny.m.delgado12@gmail.com>
2020-02-07 12:22:59 -0600

    finished practice assesment

--- solutions.js
+++ solutions.js
@@ -20,3 +20,68 @@
  * and
  * concat
  */
+
+
+// isTrue(true)             // true
+// // // isTrue(false)            // false
+// // // isTrue(0)                // false
+// // // isTrue(null)             // false
+// // // isTrue("true")           // false
+// // // isTrue("Banana")         // false
+// // // isTrue([1, 2])           // false
+
+function isTrue(input) {
+    return input === true
+}
+
+function isFalse(input) {
+    return input === false
+}
+
+
+// Define a function named not that takes in any input and returns the boolean opposite of the provided input.
+
+function not(input) {
+    return input !== typeof(input)
+}
+
+function addOne(number) {
+    return  number + 1
+}
+
+function isEven(input) {
+    if (input % 2 === 0) {
+        return true;
+    }else {
+        return false
+    }
+}
+
+function isIdentical(a, b) {
+    return a === b
+}
+
+// Define a function named isEqual that takes in two input arguments. If each argument is equal only in value, then return true. Otherwise return false.
+
+function isEqual(a, b) {
+    return a == b
+}
+
+
+function or(x, y) {
+    return x || y
+}
+
+// Define a function named and that takes in two input arguments and returns the result of a logical and operation of both inputs.
+
+function and(inputa, inputb) {
+    return inputa && inputb
+}
+
+
+// Define a function named concat that takes in two input arguments. If both arguments are strings, then return the concatenated result. If two numbers are provided, then return the string concatenation of each set of numerals
+
+
+function concat(a, b) {
+    return
+}
