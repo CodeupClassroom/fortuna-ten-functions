803398fff3a6795ab3ab4b59ad9e25efc57110ba
Andrew Ball <andy.bob.ball@gmail.com>
2020-02-07 12:22:45 -0600

    test

--- solutions.js
+++ solutions.js
@@ -1,8 +1,66 @@
-"use strict";
 
-/**
+
+
+
+function isTrue(input) {
+    return typeof input == "boolean";
+}
+isTrue();
+
+function isFalse(x) {
+    return x === 0;
+}
+isFalse();
+
+function not(input){
+    return input !== true;
+}
+not(0);
+
+function addOne(inputNumber) {
+    return inputNumber++;
+}
+addOne();
+
+function isEven(singleInput){
+    return singleInput % 2 == 0;
+}
+isEven();
+
+function isIdentical(input, input2) {
+    return input === input2;
+}
+isIdentical();
+
+function isEqual(inputOne, inputTwo){
+    return inputOne == inputTwo;
+}
+isEqual(3, "3");
+
+function or(input, input2){
+    return input || input2;
+}
+or();
+
+function and(input, input2) {
+    return input && input2;
+}
+and();
+
+function concat(input1, input2){
+    return input1 + input2;
+}
+concat();
+
+/*
  * Write your solutions here.
  *
+ * /
+
+
+
+ */
+ /*
  * **Note**: While normally it is good practice to wrap your javascript in an
  * immediately invoked function expression (iife), you should _not_ do that
  * here. If you do, the automated tests will not be able to see your functions.
