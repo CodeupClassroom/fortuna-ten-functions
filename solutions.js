f5ac1f0e6859986b4b0b81c9194fe6f094567b92
Taryn McKenzie <taryn.m.mckenzie@gmail.com>
2020-02-07 11:22:59 -0600

    #1 completed and checked

--- solutions.js
+++ solutions.js
@@ -20,3 +20,19 @@
  * and
  * concat
  */
+
+
+// 1. Define a function named `isTrue` that takes in any input and returns true if the input provided is exactly equal to `true` in value and data type.
+//
+// isTrue(true)             // true
+// isTrue(false)            // false
+// isTrue(0)                // false
+// isTrue(null)             // false
+// isTrue("true")           // false
+// isTrue("Banana")         // false
+// isTrue([1, 2])           // false
+
+function isTrue(input){
+    // equal to true in value and type
+    return input === true;
+}
